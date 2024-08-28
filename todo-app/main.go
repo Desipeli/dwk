fd42be73fd3a2b9d16cf986974d81feb4c4296bf
desipeli <mr.desipeli@gmail.com>
2024-08-28 12:22:34 +0300

    added templ, todo responds to GET /

--- todo-app/main.go
+++ todo-app/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"todo-app/internal/templates"
 )
 
 func main() {
@@ -12,7 +13,22 @@ func main() {
 		port = "8000"
 	}
 
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", homePage)
+
 	log.Printf("Server started in port: %s", port)
 	portAddr := ":" + port
-	http.ListenAndServe(portAddr, nil)
+	http.ListenAndServe(portAddr, mux)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != ("/") {
+		// catch all undefined routes
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	page := templates.HomePage()
+	page.Render(r.Context(), w)
+
 }
