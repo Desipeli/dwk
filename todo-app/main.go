2e57c40bacdcce0168e1dd91a5b428dbb296b314
desipeli <mr.desipeli@gmail.com>
2024-08-28 09:27:45 +0300

    default port 8000

--- todo-app/main.go
+++ todo-app/main.go
@@ -8,6 +8,10 @@ import (
 
 func main() {
 	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
 	log.Printf("Server started in port %s", port)
 	portAddr := ":" + port
 	http.ListenAndServe(portAddr, nil)
