36bb4283b1ac607e881b3a8e2f214e24c24c9912
desipeli <mr.desipeli@gmail.com>
2024-08-28 09:21:31 +0300

    listen and serve

--- todo-app/main.go
+++ todo-app/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 	log.Printf("Server started in port %s", port)
+	portAddr := ":" + port
+	http.ListenAndServe(portAddr, nil)
 }
